f97d26e2423d16d159b340806c8555e31ca32e0d
RLant <rebeccalant@gmail.com>
2021-03-16 15:07:38 -0400

    Adding more JS, fixing class/id on HTML

--- scripts.js
+++ scripts.js
@@ -6,11 +6,12 @@ const balance = document.getElementById("balance"),
     text = document.getElementById("text"),
     amount = document.getElementById("amount");
 
-const dummmyTransactions = [
-    {id: 1,text: "Flower", amount: -20},
-    {id: 2,text: "Salary", amount: 300},
-    {id: 3,text: "Book", amount: -10},
-    {id: 4,text: "Camera", amount: 150}
+
+let dummmyTransactions = [
+    {id: 1, text: "Flower", amount: -20},
+    {id: 2, text: "Salary", amount: 300},
+    {id: 3, text: "Book", amount: -10},
+    {id: 4, text: "Camera", amount: 150}
 ];
 
 let transactions = dummmyTransactions;
@@ -27,7 +28,19 @@ function addToTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${transaction.amount}</span>    
-    `
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn">x</button>    
+    `;
+    
+    list.appendChild(item);
 
 }
+
+// Init App
+
+function init() {
+    list.innerHTML = "";
+    transactions.forEach(addToTransactionDOM);
+}
+
+
+init();
