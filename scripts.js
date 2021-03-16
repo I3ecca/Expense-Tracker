3608b0b0aa6f76d8ec827f7fe4aef5b0cb263790
RLant <rebeccalant@gmail.com>
2021-03-15 21:34:41 -0400

    adding some JS

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const balance = document.getElementById("balance"),
+    money_plus = document.getElementById("money-plus"),
+    money_minus = document.getElementById("money-minus"),
+    list = document.getElementById("list"),
+    form = document.getElementById("form"),
+    text = document.getElementById("text"),
+    amount = document.getElementById("amount");
+
+const dummmyTransactions = [
+    {id: 1,text: "Flower", amount: -20},
+    {id: 2,text: "Salary", amount: 300},
+    {id: 3,text: "Book", amount: -10},
+    {id: 4,text: "Camera", amount: 150}
+];
+
+let transactions = dummmyTransactions;
+
+//Add transactions to DOM list
+
+function addToTransactionDOM(transaction) {
+    //get sign, if trans aount is greater than 0, then (?) we change the sign to minus, else (:) we change the sign to a plus. 
+    const sign = transaction.amount < 0 ? "-" : "+";
+
+    const item = document.createElement("li");
+
+    // Add class besed on value. If the trans amount is less that 0, add class minus, else add clas plus. 
+    item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+    item.innerHTML = `
+        ${transaction.text} <span>${sign}${transaction.amount}</span>    
+    `
+
+}
