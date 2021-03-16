ed8f6fcbe3af58f298afe1ca7b23f0b0e88b3819
RLant <rebeccalant@gmail.com>
2021-03-16 17:10:04 -0400

    adding JS value and expense updates

--- scripts.js
+++ scripts.js
@@ -35,6 +35,31 @@ function addToTransactionDOM(transaction) {
 
 }
 
+
+//Update balance, income and expense. 
+function updateValues(){
+    //sets amounts to any array of just the amounts of each transaction. 
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts
+        .reduce((acc,item) => (acc += item) , 0)
+        .toFixed(2);
+
+    const income = amounts
+        .filter(item =>  item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (amounts
+        .filter(item => item < 0 )
+        .reduce((acc,item) => (acc += item), 0) * -1)
+        .toFixed(2);
+
+        balance.innerText  =`$${total}`;
+        money_plus.innerText = `$${income}`;
+        money_minus.innerText = `$${expense}`;
+}
+
+
 // Init App
 
 function init() {
@@ -44,3 +69,6 @@ function init() {
 
 
 init();
+
+updateValues();
+
